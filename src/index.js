c50f85303abc558d66c40a926f2cd920fe11c22f
Danny McCracken <dannymccracken13@yahoo.com>
2021-03-18 15:13:53 -0500

    completed creating basic file structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router } from "react-router-dom"
+import { RecHub } from "./components/RecHub.js";
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Router>
+          <RecHub />
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
